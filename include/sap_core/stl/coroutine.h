6989b4eac3738af5ed04c7862a0bb7ce34874e24
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2026-06-19 17:01:42 +0200

    Adds stl::coroutine

---
+++ include/sap_core/stl/coroutine.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <coroutine>
+
+namespace stl {
+    using std::coroutine_handle;
+    using std::coroutine_traits;
+    using std::noop_coroutine;
+    using std::noop_coroutine_handle;
+    using std::noop_coroutine_promise;
+    using std::suspend_always;
+    using std::suspend_never;
+} // namespace stl
