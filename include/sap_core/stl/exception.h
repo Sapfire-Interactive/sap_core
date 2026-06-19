b639b283fd119468086a1d8674c2f47d33e5234b
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2026-06-19 17:01:42 +0200

    Adds stl::exception

---
+++ include/sap_core/stl/exception.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <exception>
+
+namespace stl {
+    using std::current_exception;
+    using std::exception;
+    using std::exception_ptr;
+    using std::make_exception_ptr;
+    using std::rethrow_exception;
+} // namespace stl
