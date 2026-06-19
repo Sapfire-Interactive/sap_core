eb23fe52f6c46585d84b68c8c1693f36fdea8d06
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2026-06-19 17:01:42 +0200

    Adds stl::optional

---
+++ include/sap_core/stl/optional.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <optional>
+
+namespace stl {
+    using std::make_optional;
+    using std::nullopt;
+    using std::nullopt_t;
+    using std::optional;
+} // namespace stl
