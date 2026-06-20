60540dfe2d8e95ea6515b28bb37fb36457f29f39
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2026-06-20 10:09:54 +0200

    Adds stl::in_place tags to stl::utility

--- include/sap_core/stl/utility.h
+++ include/sap_core/stl/utility.h
@@ -6,4 +6,11 @@ namespace stl {
     using std::exchange;
     using std::forward;
     using std::move;
+
+    using std::in_place;
+    using std::in_place_index;
+    using std::in_place_index_t;
+    using std::in_place_t;
+    using std::in_place_type;
+    using std::in_place_type_t;
 } // namespace stl
